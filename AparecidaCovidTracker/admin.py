6bf8c1caec40ff7e68a59fa3bee31a6562329b39
Gustavo Costa <gusbemacbe@gmail.com>
2021-06-05 18:26:41 -0300

    Reorganised folders

--- AparecidaCovidTracker/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Product, ProductAdmin
-
-admin.site.register(Product, ProductAdmin)
--- AparecidaCovidTracker/urls.py
+++
@@ -1,4 +0,0 @@
-from django.urls import path
-from .views import AparecidaCovidTrackerView
-
-urlpatterns = [ path('', AparecidaCovidTrackerView.as_view(), name='aparecida-covid-19-tracker')]
